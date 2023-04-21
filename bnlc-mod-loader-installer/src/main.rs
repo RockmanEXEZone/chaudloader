1e7939359386e1f7e0fd41ffa058a0f053f8712d
bigfarts <bigfarts@punymail.com>
2023-04-21 14:37:15 -0700

    add installer script

---
+++ bnlc-mod-loader-installer/src/main.rs
@@ -0,0 +1,102 @@
+use std::io::Write;
+
+const BANNER: &str = "
+
+        %%%%%%%%%%%%%%%%%
+     %%%%%  *********  %%%%%
+   %%%% *************     %%%%
+  %%% ***************       %%%
+ %%% *************** ******* %%%
+ %%% ************ ********** %%%    bnlc-mod-loader
+ %%% ********** ************ %%%    Installer
+ %%% ******* *************** %%%
+  %%%       *************** %%%
+   %%%%     ************* %%%%
+     %%%%%  *********  %%%%%
+        %%%%%%%%%%%%%%%%%
+";
+
+fn real_main() -> Result<(), anyhow::Error> {
+    println!("{}", BANNER);
+
+    println!("Welcome to the bnlc-mod-loader installer.");
+    println!();
+
+    let mut steamdir = steamlocate::SteamDir::locate()
+        .ok_or_else(|| anyhow::anyhow!("could not initialize steam dir"))?;
+
+    let apps = steamdir.apps();
+
+    let mut paths = vec![];
+
+    println!("Found games:");
+    if let Some(app) = apps.get(&1798010).and_then(|v| v.as_ref()) {
+        println!(" - Vol. 1: {}", app.path.display());
+        paths.push(app.path.clone())
+    }
+    if let Some(app) = apps.get(&1798020).and_then(|v| v.as_ref()) {
+        println!(" - Vol. 2: {}", app.path.display());
+        paths.push(app.path.clone())
+    }
+
+    if paths.is_empty() {
+        println!(
+            " ! Mega Man Battle Network Legacy Collection could not be detected on your computer."
+        );
+        println!();
+        println!("Installation cancelled.");
+        return Ok(());
+    }
+
+    print!("Do wish to proceed? [Y/n] ");
+    std::io::stdout().flush()?;
+    let mut response = String::new();
+    std::io::stdin().read_line(&mut response)?;
+    response = response.trim().to_lowercase();
+    if response != "y" && response != "" {
+        println!("Installation cancelled.");
+        return Ok(());
+    }
+
+    println!();
+
+    let exe_path = std::env::current_exe()?;
+    let src_path = exe_path
+        .parent()
+        .ok_or_else(|| anyhow::anyhow!("could not get parent directory"))?;
+
+    let dxgi_dll = std::fs::read(src_path.join("dxgi.dll"))?;
+    let bnlc_mod_loader_dll = std::fs::read(src_path.join("bnlc_mod_loader.dll"))?;
+
+    for path in paths {
+        let dxgi_dll_path = path.join("exe").join("dxgi.dll");
+        let mut dxgi_dll_f = std::fs::File::create(&dxgi_dll_path)?;
+        dxgi_dll_f.write_all(&dxgi_dll)?;
+        println!("OK: dxgi.dll -> {}", dxgi_dll_path.display());
+
+        let bnlc_mod_loader_dll_path = path.join("exe").join("bnlc_mod_loader.dll");
+        let mut bnlc_mod_loader_dll_f = std::fs::File::create(&bnlc_mod_loader_dll_path)?;
+        bnlc_mod_loader_dll_f.write_all(&bnlc_mod_loader_dll)?;
+        println!(
+            "OK: bnlc_mod_loader.dll -> {}",
+            bnlc_mod_loader_dll_path.display()
+        );
+    }
+    println!();
+
+    println!("Installation successful!");
+    Ok(())
+}
+
+fn main() {
+    let r = real_main();
+    println!();
+    if let Err(err) = r {
+        print!("Installation failed with error: {}", err);
+    } else {
+        print!("Press enter or close this window to finish.");
+    }
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut String::new()).unwrap();
+    return;
+}
