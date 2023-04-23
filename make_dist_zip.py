237dc638a7a1e012c3eb3b05d5f48473d40c620b
bigfarts <bigfarts@punymail.com>
2023-04-23 00:19:11 -0700

    should this mode be 777 actually?

--- make_dist_zip.py
+++ make_dist_zip.py
@@ -6,13 +6,13 @@ import zipfile
 now = datetime.datetime.now()
 
 
-def zfile(name, mode=0o644):
+def zfile(name, mode=0o666):
     zi = zipfile.ZipInfo(name, now.timetuple()[:6])
     zi.external_attr = (0o100000 | mode) << 16
     return zi
 
 
-def zdir(name, mode=0o755):
+def zdir(name, mode=0o777):
     zi = zipfile.ZipInfo(name, now.timetuple()[:6])
     zi.compress_size = 0
     zi.file_size = 0
@@ -29,7 +29,7 @@ files = [
     (zfile("lua54.dll"), "lua54.dll"),
     (zfile("install.exe"), "target/release/install.exe"),
     (
-        zfile("install-linux", 0o755),
+        zfile("install-linux", 0o777),
         "target/x86_64-unknown-linux-musl/release/install",
     ),
 ]
