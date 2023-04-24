07680a013f778468643b394ccb8d50d9cbb43fa6
bigfarts <bigfarts@punymail.com>
2023-04-23 20:50:28 -0700

    use std::mem::size_of::<u16>() instead of 2

--- chaudloader/src/assets/textarchive.rs
+++ chaudloader/src/assets/textarchive.rs
@@ -3,7 +3,7 @@ use byteorder::{ReadBytesExt, WriteBytesExt};
 pub fn unpack(mut r: impl std::io::Read) -> Result<Vec<Vec<u8>>, std::io::Error> {
     // Read offsets table.
     let first_offset = r.read_u16::<byteorder::LittleEndian>()? as usize;
-    let n = first_offset / 2;
+    let n = first_offset / std::mem::size_of::<u16>();
 
     let mut offsets = Vec::with_capacity(n);
     offsets.push(first_offset);
@@ -32,7 +32,7 @@ pub fn unpack(mut r: impl std::io::Read) -> Result<Vec<Vec<u8>>, std::io::Error>
 }
 
 pub fn pack(entries: &[&[u8]], mut w: impl std::io::Write) -> Result<(), std::io::Error> {
-    let mut offset = (entries.len() * 2) as usize;
+    let mut offset = (entries.len() * std::mem::size_of::<u16>()) as usize;
 
     // Write offsets table.
     for entry in entries.iter() {
