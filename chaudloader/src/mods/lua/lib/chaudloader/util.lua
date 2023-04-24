bdb0492cd0611ec5d6c4bbfdb6341d5895378db7
bigfarts <bigfarts@punymail.com>
2023-04-24 16:21:36 -0700

    add merge_msg convenience function

--- chaudloader/src/mods/lua/lib/chaudloader/util.lua
+++ chaudloader/src/mods/lua/lib/chaudloader/util.lua
@@ -15,13 +15,24 @@ local function edit_msg(mpak, address, cb)
     mpak[address] = chaudloader.pack_msg(cb(chaudloader.unpack_msg(mpak[address])))
 end
 
+-- Merges two messages together, preferring the latter one.
+--
+-- Only non-empty entries from the new msg data will be merged.
+local function merge_msg(old, new)
+    for i, entry in ipairs(new) do
+        if entry ~= "" then
+            old[i] = entry
+        else
+        end
+    end
+    return old
+end
+
 -- Merges all msgs from a directory.
 --
--- - The directory must contain files named addresses of msgs to replace, followed by `.msg`.
+-- The directory must contain files named addresses of msgs to replace, followed by `.msg`.
 --
---   The addresses may be either mapped ROM addresses (08XXXXXX) or unmapped file offsets (00XXXXXX): if they are unmapped file offsets, they will be automatically transformed into mapped ROM addresses.
---
--- - Non-empty entries from the source msg data will be merged into the target msg data.
+-- The addresses may be either mapped ROM addresses (08XXXXXX) or unmapped file offsets (00XXXXXX): if they are unmapped file offsets, they will be automatically transformed into mapped ROM addresses.
 local function merge_msgs_from_mod_directory(mpak, dir)
     for _, filename in ipairs(chaudloader.list_mod_directory(dir)) do
         local raw_addr = string.match(filename, "^(%x+).msg$")
@@ -29,13 +40,8 @@ local function merge_msgs_from_mod_directory(mpak, dir)
             goto continue
         end
         local addr = tonumber(raw_addr, 16) | 0x08000000
-        edit_msg(mpak, addr, function (ta)
-            for i, entry in ipairs(chaudloader.unpack_msg(chaudloader.read_mod_file(dir .. '/' .. filename))) do
-                if entry ~= "" then
-                    ta[i] = entry
-                end
-            end
-            return ta
+        edit_msg(mpak, addr, function (msg)
+            return merge_msg(msg, chaudloader.unpack_msg(chaudloader.read_mod_file(dir .. '/' .. filename)))
         end)
         ::continue::
     end
@@ -44,5 +50,6 @@ end
 return {
     edit_mpak = edit_mpak,
     edit_msg = edit_msg,
+    merge_msg = merge_msg,
     merge_msgs_from_mod_directory = merge_msgs_from_mod_directory,
 }
